fa5bfac55531b7acddca3d83a05d772c0ac93bfe
Mr.Papalitas <miaw.raja@gmail.com>
2025-08-10 10:47:11 +0700

    kontoeomffiejgigjtrignirijife

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+function showSection(sectionId) {
+  const sections = ['Home-Content', 'About-Content', 'Project-Content', 'Contact-Content'];
+  sections.forEach(id => {
+    const el = document.getElementById(id); //ngambil id dari var sections
+    if (el) {
+      el.classList.add('hidden'); //hiddenin semua div
+    }
+  });
+
+  const target = document.getElementById(sectionId); //ambil ud dari parameter function
+  if (target) {
+    target.classList.remove('hidden'); //rmove property hidden di element id
+  }
+}
+// Home()
+About()
+// Project();
+// Contact();
+function Home() {
+  showSection('Home-Content');
+}
+
+function About() {
+  showSection('About-Content');
+}
+
+function Project() {
+  showSection('Project-Content');
+}
+
+function Contact() {
+  showSection('Contact-Content');
+}
